8e16049d2caf44ed48549de925aa50b283bafac8
oabrivard <olivier@abrivard.fr>
2020-08-05 10:08:56 -0400

    updated phonebook-backend

---
+++ build/precache-manifest.f1646802ef81b7750b7b460711db8eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4a4aa4432b3b676bc0efd47eb3a148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7f08e3295ec9f4dcf1",
+    "url": "/static/css/main.685aab4e.chunk.css"
+  },
+  {
+    "revision": "05945b357eb1f5da6df3",
+    "url": "/static/js/2.40776c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40776c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7f08e3295ec9f4dcf1",
+    "url": "/static/js/main.68962016.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
